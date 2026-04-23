db4de02f7d24991ebe322e571898a935657e13f1
Saja <72330059@students.liu.edu.lb>
2026-04-23 11:44:58 +0300

    Initial Commit

--- app/src/main/java/com/example/crises/Services.java
+++ app/src/main/java/com/example/crises/Services.java
@@ -4,7 +4,7 @@ import android.app.AlertDialog;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.widget.Toast;
+
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
