35f29602f8bc8bcb30d1931ec620c50cae357afe
Douaa Mourtada <mourtadadouaa@gmail.com>
2026-05-12 02:32:15 +0300

    Add files via upload

---
+++ actions/get_users.php
@@ -0,0 +1,8 @@
+<?php
+require_once("../class/DAL.class.php");
+header("Content-Type: application/json");
+
+$dal = new DAL();
+$users = $dal->getdata("SELECT id, name FROM users WHERE 1");
+
+echo json_encode($users);
