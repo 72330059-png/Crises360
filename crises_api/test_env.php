e68533401a9eb7c47e79350ad42e5615fc1a6445
Sajaayoub <72330059@students.liu.edu.lb>
2026-06-26 15:39:21 +0300

    Update test_env.php

--- crises_api/test_env.php
+++ crises_api/test_env.php
@@ -1,8 +1,8 @@
 <?php
 header('Content-Type: application/json');
 echo json_encode([
-    'MAIL_FROM'     => getenv('MAIL_FROM'),
-    'MAIL_NAME'     => getenv('MAIL_NAME'),
+    'MAIL_USERNAME' => getenv('MAIL_USERNAME'),
+    'MAIL_HOST'     => getenv('MAIL_HOST'),
     'MAIL_PASSWORD' => getenv('MAIL_PASSWORD') ? 'SET' : 'NOT SET',
     'php_version'   => phpversion()
 ]);
