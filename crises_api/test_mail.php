3cb30f5097f17cb2eaddc3894ab56ff9d48fd6c7
Sajaayoub <72330059@students.liu.edu.lb>
2026-06-26 15:48:08 +0300

    Add PHPMailer includes to test_mail.php

--- crises_api/test_mail.php
+++ crises_api/test_mail.php
@@ -1,4 +1,9 @@
 <?php
 header('Content-Type: application/json');
-echo json_encode(['status' => 'alive']);
+
+require_once 'PHPMailer/Exception.php';
+require_once 'PHPMailer/PHPMailer.php';
+require_once 'PHPMailer/SMTP.php';
+
+echo json_encode(['status' => 'includes ok']);
 ?>
